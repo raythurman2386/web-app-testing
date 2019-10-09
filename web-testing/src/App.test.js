7df34dce8ef5238891ad2e254c14aaa451dbf897
raythurman2386 <raymondthurman5@gmail.com>
2019-10-09 17:26:39 -0400

    minor changes, switching laptops

--- web-testing/src/App.test.js
+++ web-testing/src/App.test.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import * as rtl from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import App from './App'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
