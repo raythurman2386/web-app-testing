64db33131588230d1e0ab6efb95ff03c6be07274
raythurman2386 <raymondthurman5@gmail.com>
2019-10-10 18:48:28 -0400

    added a snapshot test to play around with them

--- web-testing/src/App.test.js
+++ web-testing/src/App.test.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import * as rtl from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import renderer from 'react-test-renderer'
 import App from './App'
+import Dashboard from './components/Dashboard'
+import Display from './components/Display'
 
 afterEach(rtl.cleanup)
 
-it('renders "all about kittens" text', () => {
-  const wrapper = rtl.render(<App />)
-  console.log(wrapper.debug())
+describe('My Test Suite', () => {
+  it('Renders the App component', () => {
+    const wrapper = rtl.render(<App />)
+    // assertions will be here
+    expect(wrapper.findByText(/at bat/i))
+  })
 
-  // query
-  const hasText = wrapper.getByText(/display/i)
-  // assertions will be here
-  expect(hasText).toBeVisible()
+  it('Checks how many buttons there are', () => {
+    const wrapper = rtl.render(<Dashboard />)
+    const checkButtons = wrapper.getAllByRole('button')
+
+    // assertion
+    expect(checkButtons.length).toBe(4)
+  })
+
+  it('should snapshot renders', () => {
+    const component = renderer.create(<Display ball={0} />)
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 })
