3b26b59897370d2e198341e37e7055e2b5aa7694
raythurman2386 <raymondthurman5@gmail.com>
2019-10-10 17:25:36 -0400

    initial state setup

--- web-testing/src/App.js
+++ web-testing/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Display from './components/Display'
 import Dashboard from './components/Dashboard'
 
 function App() {
+  const [ball, setBall] = useState(0)
+  const [strike, setStrike] = useState(0)
+
   return (
     <div>
       <Display />
